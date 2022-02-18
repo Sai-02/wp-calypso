774bfd1c6515bd0934408c7cb402845691e61e2d
Christopher Smith <chris@chriswsmith.info>
2022-02-17 20:24:53 -0700

    Re-add card prop.

--- client/me/purchases/payment-methods/payment-method-details.tsx
+++ client/me/purchases/payment-methods/payment-method-details.tsx
@@ -6,6 +6,7 @@ import { useLocalizedMoment } from 'calypso/components/localized-moment';
 import {
 	getPaymentMethodImageURL,
 	getPaymentMethodSummary,
+	PaymentMethod,
 } from 'calypso/lib/checkout/payment-methods';
 
 import 'calypso/me/purchases/payment-methods/style.scss';
@@ -19,6 +20,7 @@ interface Props {
 	paymentPartner?: string;
 	selected?: boolean;
 	isExpired?: boolean;
+	card: PaymentMethod;
 }
 
 const PaymentMethodDetails: FunctionComponent< Props > = ( {
